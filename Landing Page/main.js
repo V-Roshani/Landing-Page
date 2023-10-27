080c5952b4a5fd58a44f33b67ca0ab79789b2dee
Roshani Vivekanantham <90918975+V-Roshani@users.noreply.github.com>
2023-10-27 22:15:54 +0530

    Add files via upload

---
+++ Landing Page/main.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
